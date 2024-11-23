1fd71a09e8d778336e74b8fe3d7a4570a7b0f05d
Pratik Panchal <pratikpanchal20040502@gmail.com>
2024-11-23 14:28:16 +0530

    update image

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -7,15 +7,12 @@ const userValidationSchema = joi.object({
     username: joi.string().min(3).max(15),
     email: joi.string().email().required(),
     password: joi.string().required().min(4),
-    profileImage: joi.any()
 })
 const createUser = async (req, res) => {
     try {
         const { username, email, password } = req.body;
         const { error } = userValidationSchema.validate(req.body);
 
-        const file = req.file
-        if (!file) return errorResponse(res, "Profile image is required");
         if (error) return errorResponse(res, error.message);
 
         const userAlreadyExists = await User.findOne({ username });
@@ -24,11 +21,8 @@ const createUser = async (req, res) => {
         const existingEmail = await User.findOne({ email });
         if (existingEmail) return errorResponse(res, "Email already exists");
 
-        const profileImageUrl = await uploadOnCloudinry(file.path)
-        if (!profileImageUrl) return errorResponse(res, "Image not uploaded on cloudinary");
-
         const token = generateAccessToken({ username, email })
-        const user = await User.create({ username, email, password, token, profileImage: profileImageUrl });
+        const user = await User.create({ username, email, password, token });
         if (!user) return errorResponse(res, "User not created");
 
         const userResponse = user.toObject();
